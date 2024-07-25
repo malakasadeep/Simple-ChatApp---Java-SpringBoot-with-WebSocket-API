2a18172a154254c60703873b5f17b75792652868
malakasadeep <151869451+malakasadeep@users.noreply.github.com>
2024-07-25 22:43:18 +0530

    innitial commit

---
+++ src/main/java/com/example/chatapplication/chat/ChatMessage.java
@@ -0,0 +1,20 @@
+package com.example.chatapplication.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+}
